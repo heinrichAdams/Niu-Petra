3124dbae08332257066e29e4d0f8fb1c595a83f9
Heinrich Adams <126666569+heinrichAdams@users.noreply.github.com>
2024-03-09 17:45:52 +0000

    Combat Animations, Working Parralax Backgroun, Transition back to main level, dungeon level killbox,

--- Scenes/Dungeon/player_in_dungeon.gd
+++ Scenes/Dungeon/player_in_dungeon.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+
 const JUMP_VELOCITY = -600.0
 
 
@@ -11,7 +12,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # ENUMS
 enum STATE{
 	RUN,
-	IDLE
+	IDLE,
+	ATTACK
 }
 enum DIRECTION{
 	LEFT,
@@ -25,8 +27,13 @@ var state : STATE = STATE.IDLE
 var direction : DIRECTION = DIRECTION.LEFT
 @onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
 @onready var selected_item = $Camera2D/selected_item
+@onready var animation_player = $AnimationPlayer
+@onready var weapon = $Weapon
 # ENDOF GLOBAL VARS
 
+
+func _ready():
+	weapon.visible = false
 
 ### _physics_process ###
 func _physics_process(delta):
@@ -60,6 +67,9 @@ func player_movement(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
+		
+	if(Input.is_action_just_pressed("LMB_click")):
+		state = STATE.ATTACK
 	
 
 	# ENDOF INPUT
@@ -80,8 +90,17 @@ func player_animation():
 					animated_sprite_2d.play("left_idle")
 				DIRECTION.RIGHT:
 					animated_sprite_2d.play("right_idle")
-
+		STATE.ATTACK:
+			match direction:
+				DIRECTION.LEFT:
+					animation_player.play("attack_left")
+				DIRECTION.RIGHT:
+					animation_player.play("attack_right")
 ### ENDOF player_animation ###
+
 
 func set_temporary_hotbar_label(new_text):
 	selected_item.text = new_text
+
+
+
